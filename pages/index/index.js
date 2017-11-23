34bf47a5fb65e80cfd217179f462cd2ce50032f1
beautifulgirlxuxu <804316947@qq.com>
2017-11-23 16:57:18 +0800

    global数据请求

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,13 @@ const app = getApp()
 
 Page({
   data: {
+    slides: null
   },
   onLoad () {
+    // 全局数据 globalData
+    // console.log(app.globalData.slides)
+    this.setData({
+      slides: app.globalData.slides
+    });
   }
 })
